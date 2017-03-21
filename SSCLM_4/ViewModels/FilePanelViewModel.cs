cb461deb97e211e99351412ab96c4b91c3482fde
Matthew Leslie <mrossleslie@gmail.com>
2017-03-21 16:28:33 -0500

    Changing around MVVM

--- SSCLM_4/ViewModels/FilePanelViewModel.cs
+++ SSCLM_4/ViewModels/FilePanelViewModel.cs
@@ -1,31 +1,61 @@
 ﻿using System;
-using System.Windows.Threading;
 using System.Windows.Input;
 using System.Runtime.InteropServices;
+using System.Windows.Interop;
 using HNTR.Models;
 
 namespace HNTR.ViewModels {
 	public class FilePanelViewModel {
-		[DllImportAttribute ("user32.dll")]
-		static extern int SendMessage (IntPtr hWnd,
-						 int Msg, int wParam, int lParam);
-		[DllImportAttribute ("user32.dll")]
-		static extern bool ReleaseCapture ();
-
-		[DllImport ("user32.dll")]
-		static extern IntPtr SendMessage (IntPtr hWnd, int msg,
-			IntPtr wParam, IntPtr lParam);
-		const int WM_POPUPSYSTEMMENU = 0x313;
-
 		public DocumentModel Document { get; private set; }
-		public ICommand DragTopPanel { get; }
+		public ICommand DragCommand { get; }
+
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HT_CAPTION = 0x2;
+
+		[DllImportAttribute ("user32.dll")]
+		public static extern int SendMessage (IntPtr hWnd, int Msg, int wParam, int lParam);
+		[DllImportAttribute ("user32.dll")]
+		public static extern bool ReleaseCapture ();
+
+		public FilePanelViewModel (DocumentModel document) {
+			Document = document;
+			DragCommand = new RelayCommand (DragWindow);
+			//File.RequestClose += (s, e) => Dispatcher.Invoke (this.Close);
+			//File.RequestMinimize += (s, e) => Dispatcher.Invoke (() => { this.WindowState = WindowState.Minimized; });
+		}
+
+		private void DragWindow() {
+			//Debug.WriteLine ("Hello World");
+			//Window.DragMove();
+
+			ReleaseCapture ();
+			SendMessage (new WindowInteropHelper (this).Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+		}
+
+		/*public ICommand CloseCommand { get; }
+		public ICommand MinimizeCommand { get; }
+
+		public event EventHandler<EventArgs> RequestClose;
+		public event EventHandler<EventArgs> RequestMinimize;
 
 		public FilePanelViewModel (DocumentModel document) {
 			Document = document;
 		}
 
-		private void PanelMovement (MouseEventArgs e) {
-			/*Dispatcher.BeginInvoke (new  (delegate () {
+		private void SetupCommands () {
+			CloseCommand = new RelayCommand (CloseApplication);
+			MinimizeCommand = new RelayCommand (MinimizeApplication);
+		}
+
+		private void MinimizeApplication (object obj) {
+			RequestMinimize (this, new EventArgs ());
+		}
+		private void CloseApplication (object obj) {
+			RequestClose (this, new EventArgs ());
+		} */
+
+		/*private void PanelMovement (MouseEventArgs e) {
+			Dispatcher.BeginInvoke (new  (delegate () {
 				base.OnMouseDown (e);
 
 				if (e.Button == MouseButtons.Left) {
@@ -35,7 +65,7 @@ namespace HNTR.ViewModels {
 					var p = MousePosition.X + (MousePosition.Y * 0x10000);
 					SendMessage (this.Handle, WM_POPUPSYSTEMMENU, (IntPtr)0, (IntPtr)p);
 				}
-			}));*/
-		}
+			}));
+		}*/
 	}
 }
--- SSCLM_4/ViewModels/MainViewModel.cs
+++ SSCLM_4/ViewModels/MainViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using HNTR.Models;
+using System.Windows.Threading;
 
 namespace HNTR.ViewModels {
 	class MainViewModel {
 		private DocumentModel _document;
-		public FilePanelViewModel File { get; set; }
+		public FilePanelViewModel File { get; private set; }
 
 		public MainViewModel () {
 			_document = new DocumentModel();
-			File = new FilePanelViewModel(_document);
+			/*File = new FilePanelViewModel(_document);
+			File.RequestClose += (s, e) => Dispatcher.Invoke (this.Close);
+			File.RequestMinimize += (s, e) => Dispatcher.Invoke (() => { this.WindowState = WindowState.Minimized; });*/
 		}
 	}
 }
