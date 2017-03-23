de6783f9d5852e068458076cec41d93cf8640138
Matthew Leslie <mrossleslie@gmail.com>
2017-03-23 07:56:04 -0500

    Removed namespaces from Constants class

--- SSLM_4/Models/Constants.cs
+++ SSLM_4/Models/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HNTR.Models {
+﻿namespace HNTR.Models {
 	public static class Constants {
 		public const string MAIN_TITLE = "SSLM (Secure Subversion Login Manager)";
 	}
