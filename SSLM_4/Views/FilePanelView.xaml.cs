721858f9f41e0f9d378c4604c604b6a49f427384
Matthew Leslie <mrossleslie@gmail.com>
2017-03-24 21:48:09 -0500

    Messing with topPanel

--- SSLM_4/Views/FilePanelView.xaml.cs
+++ SSLM_4/Views/FilePanelView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace HNTR.Views
 {
@@ -11,5 +16,37 @@ namespace HNTR.Views
         {
             InitializeComponent();
 		}
-    }
+
+		//private void Grid_MouseEnter(object sender, MouseEventArgs e)
+		//{
+		//	ColorAnimation ca = new ColorAnimation(Colors.Blue, new Duration(TimeSpan.FromSeconds(4)));
+		//	grid1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF141417"));
+		//	grid1.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+		//	MessageBox.Show("Temp"); //FF141417
+		//}
+
+		//private void exitButton_MouseEnter(object sender, MouseEventArgs e)
+		//{
+		//	ColorAnimation ca = new ColorAnimation(Colors.DarkRed, new Duration(TimeSpan.FromMilliseconds(250)));
+		//	exitButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00000000"));
+		//	exitButton.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+		//}
+
+		//private void exitButton_MouseLeave(object sender, MouseEventArgs e)
+		//{
+		//	ColorAnimation ca = new ColorAnimation((Color)ColorConverter.ConvertFromString("#00000000"), 
+		//										          new Duration(TimeSpan.FromMilliseconds(250)));
+		//	exitButton.Background = new SolidColorBrush(Colors.DarkRed);
+		//	exitButton.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+		//}
+
+
+		//private void exitButton_MouseDown(object sender, MouseButtonEventArgs e)
+		//{
+		//	ColorAnimation ca = new ColorAnimation((Color)ColorConverter.ConvertFromString("#FFE53E3E"), 
+		//												  new Duration(TimeSpan.FromMilliseconds(100)));
+		//	exitButton.Background = new SolidColorBrush(Colors.DarkRed);
+		//	exitButton.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+		//}
+	}
 }
